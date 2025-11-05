21ea5658c9c9ad0b5114b908af3e7dcd6b73c430
Detti <bernadett1998@gmail.com>
2025-11-05 14:10:41 +0100

    fix css in coach bio, and delete user button

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -4,6 +4,8 @@
     <title>@yield('title', 'GymWare')</title>
     <link rel="icon" href="{{ asset('images/dumbbell-svgrepo-com.svg') }}" type="image/png">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" rel="stylesheet">
+    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
+
 </head>
 <body class="min-h-screen flex flex-col bg-black">
 <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
