e3af1ea5391eaba9d22f186173626c092cc94fd4
Török Péter <183863587+peti9406@users.noreply.github.com>
2025-11-06 19:52:36 +0100

    done

--- resources/views/components/exercise/card.blade.php
+++ resources/views/components/exercise/card.blade.php
@@ -1,11 +1,13 @@
 <form method="POST" action="/workout-planner/exercise/{{ $exercise['id'] }}"
-      class="flex-grow items-center w-full bg-white/5 rounded-lg p-5 transition border border-white/10">
+      class="flex-grow items-center w-full bg-white/5 rounded-lg p-5 transition border border-white/10"
+      class="delete-form">
     @csrf
     @method('DELETE')
 
     <div class="flex gap-5 justify-start items-center">
         <div class="w-32 h-32 bg-white rounded-xl overflow-hidden flex-shrink-0">
-            <img src="{{ $exercise['data']['gifUrl'] }}" alt="picture of the exercise" class="w-full h-full object-cover">
+            <img src="{{ $exercise['data']['gifUrl'] }}" alt="picture of the exercise"
+                 class="w-full h-full object-cover">
         </div>
         <div class="flex flex-col ml-4 text-gray-100">
             <p class="text-md">
@@ -15,9 +17,16 @@
         </div>
         <div class="ml-auto border-l border-white/20 pl-5">
             <input type="hidden" name="plan-id" value="{{ $plan['id'] }}">
-            <button class="cursor-pointer text-red-600 hover:text-red-400 transition">
+            <button type="button" class="cursor-pointer text-red-600 hover:text-red-400 transition"
+                    onclick="openConfirmModal(event)">
                 <i class="fa-solid fa-trash"></i>
             </button>
         </div>
     </div>
+
+    <x-confirm-modal
+        question="Are you sure you want to delete this exercise?"
+        confirm="Delete"
+        cancel="Cancel">
+    </x-confirm-modal>
 </form>
