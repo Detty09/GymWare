056e206fa6f6e4900088bd9701ac2549a56d0dc9
Detty09 <190375545+Detty09@users.noreply.github.com>
2026-05-11 15:22:20 +0200

    Update ProfileController.php

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -109,7 +109,6 @@ class ProfileController extends Controller
 
         $user->save();
 
-        // 🧠 If this user is a coach, update or create the coach bio
         if ($user->is_coach) {
             Coach::updateOrCreate(
                 ['user_id' => $user->id],
